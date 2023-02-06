eb7d4742df09d428e030ededaad892a985d35612
Bipin Gouda <bipingouda1199@gmail.com>
2023-02-06 11:35:22 +0530

    init

---
+++ Tree's/Tree_creation.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+struct node{
+   
+   int data;
+   node*left;
+   node*right;
+
+   node (int val){    // a constructor
+    data=val;
+    left=NULL;
+    right=NULL;
+   }
+};
+
+int main()
+{ 
+    struct node *root= NULL;    //node type pointer to root
+
+    struct node *new_node
+    
+    return 0;
+}
