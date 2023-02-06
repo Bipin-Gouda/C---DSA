8f92b399dc36e3922d3fde2fa73dc165bba9f366
Bipin Gouda <bipingouda1199@gmail.com>
2023-02-06 22:13:41 +0530

    si

--- Linked-List/LLcreation.cpp
+++ Linked-List/LLcreation.cpp
@@ -13,5 +13,35 @@ class node{
 };
 
 void insertAtEnd(node* &head, int val){
-    
+
+    node* n = new node(val);
+
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node* temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void display(node* head){  //by value head passed
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+
+int main(){
+
+    node* head=NULL;
+    insertAtEnd(head,1);
+    insertAtEnd(head,2);
+    display(head);
+
 }
